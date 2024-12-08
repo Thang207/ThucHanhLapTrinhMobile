15cde1b295dcf6bcb03be9d8cd84c79177025ead
Thang <22521334@gm.uit.edu.vn>
2024-12-08 16:51:30 +0700

    feat: use android 7.1 only

--- Lab5/BTH5_2/app/src/main/java/com/example/bth5_2/MainActivity.java
+++ Lab5/BTH5_2/app/src/main/java/com/example/bth5_2/MainActivity.java
@@ -6,26 +6,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private PowerStateChangeReceiver receiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        receiver = new PowerStateChangeReceiver();
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        IntentFilter filter = new IntentFilter(Intent.ACTION_POWER_CONNECTED);
-        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        registerReceiver(receiver, filter);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        unregisterReceiver(receiver);
     }
 }
